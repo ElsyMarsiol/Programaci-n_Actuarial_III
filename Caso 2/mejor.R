dac721b2edff9072f81f2c2549d7285bb46c8486
ElsyMarisol <marisolnena@hotmail.es>
2017-06-01 22:51:31 -0500

    mejor.R

--- Caso 2/mejor.R
+++ Caso 2/mejor.R
@@ -6,30 +6,30 @@ mejor <- function(state,result){
     v <- c("infarto", "falla", "neumonia")
     
     if (state %in% x == F){
-        stop("estado inválido")
+        stop("estado invÃ¡lido")
         break
     }
     if (result == "infarto") r <- 11
     else if (result == "falla") r <- 17
     else if (result == "neumonia") r <- 23
     else if (result %in% v == F){
-        stop("resultado inválido")
+        stop("resultado invÃ¡lido")
         break
     }
     mydata <- data[data$State == state,]
-    mnd <- mydata[,c(2,r)]
-    if (sum(mnd[,2]=="Not Available") < 1) {
-        out <- mnd[order(as.numeric(mnd[,2])),]
-        out2 <- out[which(out[,2] == out[1,2]),]
-        fo <- out2[order(out2[,1]),]
+    verde <- mydata[,c(2,r)]
+    if (sum(verde[,2]=="Not Available") < 1) {
+        des <- verde[order(as.numeric(verde[,2])),]
+        des2 <- des[which(des[,2] == des[1,2]),]
+        fo <- des2[order(des2[,1]),]
         fo[1,1]
         
     }
     else {
-        final <- mnd[- grep("Not", mnd[,2]),]
-        out <- final[order(as.numeric(final[,2])),]
-        out2 <- out[which(out[,2] == out[1,2]),]
-        fo <- out2[order(out2[,1]),]
+        final <- verde[- grep("Not", verde[,2]),]
+        des <- final[order(as.numeric(final[,2])),]
+        des2 <- des[which(des[,2] == des[1,2]),]
+        fo <- des2[order(des2[,1]),]
         fo[1,1]
     }
 }
